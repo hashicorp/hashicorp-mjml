a4788b37e3ad7fc76d3fc6651eacf7d460bd89e4
Ryan <rmfarrell@gmail.com>
2018-05-13 22:12:29 -0400

    replace hr with section-body border

--- components/section-body.js
+++ components/section-body.js
@@ -5,13 +5,15 @@ export default class SectionBody extends BodyComponent {
     margin: 'px',
     padding: 'px',
     'background-color': 'color',
-    width: 'px'
+    width: 'px',
+    border: 'boolean'
   }
 
   static defaultAttributes = {
     margin: '0 auto 20px',
     padding: '0 25px 20px',
-    'background-color': 'white'
+    'background-color': 'white',
+    border: false
   }
 
   getStyles() {
@@ -21,7 +23,8 @@ export default class SectionBody extends BodyComponent {
         'background-color': this.getAttribute('background-color'),
         width: this.getAttribute('width'),
         margin: this.getAttribute('margin'),
-        overflow: 'hidden'
+        overflow: 'hidden',
+        'border-top': this.getAttribute('border') ? '7px solid #1563FF' : '0px'
       }
     }
   }
